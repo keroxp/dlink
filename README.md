5d22620422f0e7b985444ecabbb00405ebfced8f
keroxp <kerokerokerop@gmail.com>
2019-09-15 04:41:01 +0900

    ver: v0.2.0

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # dink
 
 Deno module linker
+
+# Concept
+
+`dink` is designed to manage URL module specifiers for [Deno](https://deno.land).
+
+Managing URL module specifier in Deno is qutie hard. dink is designed to resolve these problems by familiar way for developers.
+
+# Install
+
+```bash
+$ deno intall dink https://denopkg.com/keroxp/dink/main.ts
+```
+
+# Usage
+
+## 1. Create `modules.json` file
+
+```json
+{
+  "https://deno.land/std": {
+    "version": "@v0.17.0",
+    "modules": ["/fs/mod.ts", "/fs/path.ts", "/flags/mod.ts"]
+  }
+}
+```
+
+## 2. Run `dink` command
+
+```bash
+$ dink -A
+Linked: https://deno.land/std@v0.17.0/fs/mod.ts -> ./vendor/https/deno.land/std/fs/mod.ts
+Linked: https://deno.land/std@v0.17.0/fs/path.ts -> ./vendor/https/deno.land/std/fs/path.ts
+Linked: https://deno.land/std@v0.17.0/flags/mod.ts -> ./vendor/https/deno.land/std/flags/mod.ts
+```
+
+dink will automatically create module asias files that are described in `modules.json`. If there are `modules.json` like below:
+
+```json
+{
+  ":moduleId": {
+    "version": ":version",
+    "modules": [":moduleFile"]
+  }
+}
+```
+
+- Alias for `":modileFile"` will be created at: `./vendor/{:moduleId}/{:moduleFile}`
+- Aalis is contains: `export * from "{:moduleId}{:version}{:moduleFile}"`
+
+### 3. Import module aliases
+
+```ts
+import * fs from "./vendor/https/deno.land/std/fs/mod.ts
+```
--- main.ts
+++ main.ts
@@ -1,6 +1,36 @@
 #!/usr/bin/env deno --allow-write --allow-read
 import * as path from "./vendor/https/deno.land/std/fs/path.ts";
+import * as fs from "./vendor/https/deno.land/std/fs/mod.ts";
+import * as flags from "./vendor/https/deno.land/std/flags/mod.ts";
+import { green } from "./vendor/https/deno.land/std/fmt/colors.ts";
 import { Modules } from "./mod.ts";
+
+function isDinkModules(x, errors: string[]): x is Modules {
+  if (typeof x !== "object") {
+    errors.push("is not object");
+    return false;
+  }
+  for (const k in x) {
+    if (x.hasOwnProperty(k)) {
+      const m = x[k];
+      if (typeof m["version"] !== "string") {
+        errors.push('"version" must be string');
+        return false;
+      }
+      if (!Array.isArray(m["modules"])) {
+        errors.push(`\"modules\" must be array`);
+        return false;
+      }
+      for (const mod of m["modules"]) {
+        if (typeof mod !== "string") {
+          errors.push(`"content of "modules" must be string`);
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
 
 async function ensure(modules: Modules) {
   const encoder = new TextEncoder();
@@ -10,22 +40,78 @@ async function ensure(modules: Modules) {
     const scheme = protocol.slice(0, protocol.length - 1);
     const dir = path.join("./vendor", scheme, hostname, pathname);
     const writeLinkFile = async (mod: string) => {
-      const modFile = `${dir}${mod}`;
+      const modFile = path.join(dir, mod);
       const modDir = path.dirname(modFile);
       await Deno.mkdir(modDir, true);
-      const specifier = `${k}${v.version}${mod}`;
+      const specifier = `${k}${path.join(v.version, mod)}`;
       const link = `export * from "${specifier}";\n`;
       const f = await Deno.open(modFile, "w");
       await Deno.write(f.rid, encoder.encode(link)).finally(() => f.close());
-      console.log(`Linked: ${specifier}`);
+      console.log(`${green("Linked")}: ${specifier} -> ./${modFile}`);
     };
     await Promise.all(v.modules.map(writeLinkFile));
   }
 }
+
+const VERSION = "0.2.0";
+
+type DinkOptions = {
+  file?: string;
+};
 async function main() {
-  const file = await Deno.readFile("./modules.json");
+  const args = flags.parse(Deno.args, {
+    alias: {
+      h: "help",
+      V: "ver"
+    },
+    "--": true
+  });
+  if (args["V"] || args["ver"]) {
+    console.log(VERSION);
+    Deno.exit(0);
+  }
+  if (args["h"] || args["help"]) {
+    console.log(
+      String(`
+    USAGE
+
+      dink -A
+        or
+      dink --allow-write --allow-read 
+
+    ARGUMENTS
+
+    OPTIONS
+   
+      -f, --file         Custom path for module.json  (Optional)         
+       
+    GLOBAL OPTIONS
+    
+      -h, --help         Display help
+      -V, --ver          Display version
+         
+    `)
+    );
+    Deno.exit(0);
+  }
+  const opts: DinkOptions = {
+    file: "./modules.json"
+  };
+  if (args["f"]) {
+    opts.file = args["f"];
+  }
+  if (!(await fs.exists(opts.file))) {
+    console.error(`${opts.file} does not exists`);
+    Deno.exit(1);
+  }
+  const file = await Deno.readFile(opts.file);
   const decoder = new TextDecoder();
   const json = JSON.parse(decoder.decode(file)) as Modules;
+  const errors = [];
+  if (!isDinkModules(json, errors)) {
+    console.error(`${opts.file} has syntax error: ${errors.join(",")}`);
+    Deno.exit(1);
+  }
   await ensure(json);
 }
 
